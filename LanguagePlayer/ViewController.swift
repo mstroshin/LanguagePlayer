7d56e29ccc989297e07353ccb3b63aae257c76a8
Maxim Troshin <mstroshin@gmail.com>
2020-07-16 15:52:35 +0300

    Initial commit

---
+++ LanguagePlayer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LanguagePlayer
+//
+//  Created by Maxim Troshin on 16.07.2020.
+//  Copyright © 2020 Maxim Troshin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
