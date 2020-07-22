897bb9206a3febacf30167144e0ed3ba79e91a15
Maxim Troshin <mstroshin@gmail.com>
2020-07-22 06:17:12 +0300

    Added PlayerControls UI

---
+++ LanguagePlayer/UserStories/VideoPlayer/ControlsView.swift
@@ -0,0 +1,82 @@
+//
+//  ControlsView.swift
+//  LanguagePlayer
+//
+//  Created by Maxim Troshin on 22.07.2020.
+//  Copyright © 2020 Maxim Troshin. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+protocol ControlsViewDelegate: class {
+    func didPressClose()
+    func didPressBackwardFifteen()
+    func didPressForwardFifteen()
+    func didPressPlay()
+    func didPressPause()
+    func didPressScreenTurn()
+    func seekValueChangedSeekSlider(time: TimeInterval)
+}
+
+class ControlsView: UIView {
+    weak var delegate: ControlsViewDelegate?
+    var isPlaying = false {
+        didSet {
+            if isPlaying {
+                self.playPauseButton.setImage(UIImage(systemName: "pause"), for: .normal)
+            } else {
+                self.playPauseButton.setImage(UIImage(systemName: "play"), for: .normal)
+            }
+        }
+    }
+    
+    @IBOutlet private weak var closeButton: UIButton!
+    @IBOutlet private weak var backwardFifteenButton: UIButton!
+    @IBOutlet private weak var forwardFifteenButton: UIButton!
+    @IBOutlet private weak var playPauseButton: UIButton!
+    @IBOutlet private weak var seekSlider: UISlider!
+    @IBOutlet private weak var timeLabel: UILabel!
+    @IBOutlet private weak var screenTurnButton: UIButton!
+    
+    @IBAction private func didPressCloseButton(_ sender: UIButton) {
+        self.delegate?.didPressClose()
+    }
+    
+    @IBAction private func didPressBackwardFifteenButton(_ sender: UIButton) {
+        self.delegate?.didPressBackwardFifteen()
+    }
+    
+    @IBAction private func didPressForwardFifteenButton(_ sender: UIButton) {
+        self.delegate?.didPressForwardFifteen()
+    }
+    
+    @IBAction private func didPressPlayPauseButton(_ sender: UIButton) {
+        if self.isPlaying {
+            self.delegate?.didPressPause()
+        } else {
+            self.delegate?.didPressPlay()
+        }
+    }
+    
+    @IBAction private func didPressScreenTurnButton(_ sender: UIButton) {
+        self.delegate?.didPressScreenTurn()
+    }
+    
+    @IBAction private func valueChangedSeekSlider(_ sender: UISlider) {
+        self.delegate?.seekValueChangedSeekSlider(time: TimeInterval(sender.value))
+    }
+    
+    func set(timeInSeconds: TimeInterval) {
+        let formatter = DateComponentsFormatter()
+        formatter.zeroFormattingBehavior = .pad
+        formatter.allowedUnits = [.hour, .minute, .second]
+        formatter.unitsStyle = .full
+
+        let formattedString = formatter.string(from: timeInSeconds)!
+        self.timeLabel.text = formattedString
+        
+        print(formattedString)
+    }
+    
+}
