f4fa59a1a471dd3272c6eb134f8ca2d6a181f1a1
Maxim Troshin <mstroshin@gmail.com>
2020-08-05 14:22:23 +0300

    web server is working (almost)

--- LanguagePlayer/html/js/index.js
+++ LanguagePlayer/html/js/index.js
@@ -22,14 +22,14 @@ class API {
             url: url,
             data: data
         })
-        .done(function (json) {
-            callback(json);
-        })
-        .fail(function (json) {
-            if (error) {
-                error(json);
-            }
-        });
+            .done(function (json) {
+                callback(json);
+            })
+            .fail(function (json) {
+                if (error) {
+                    error(json);
+                }
+            });
     }
 }
 
@@ -59,79 +59,31 @@ function renderVideosTable() {
 }
 
 function removeVideo(id) {
-    api.remove(id, function() {
+    api.remove(id, function () {
         $("#video-" + id).remove();
     });
 }
 
-function fileSelected(element) {
-//    var input = $(element);
-//    var html = input.parent().html().trim();
-//    var inputString = " <input" + html.split("<input")[1];
-//    input.parent().html(input.val() + inputString);
-}
+function onSubmitUpload() {
+    let uploadForm = $("#uploadForm")[0];
+    // let progressBar = $("#myBar");
 
-$(function(){
-    var ul = $('#upload ul');
+    var request = new XMLHttpRequest();
+    request.upload.onprogress = function (e) {
+        console.log("progress " + e.loaded + "/" + e.total);
 
-    $('#upload').fileupload({
-        add: function (e, data) {
-
-            var tpl = $('<li class="working"><input type="text" value="0" data-width="48" data-height="48"'+
-                ' data-fgColor="#0788a5" data-readOnly="1" data-bgColor="#3e4043" /><p></p><span></span></li>');
-
-            tpl.find('p').text(data.files[0].name)
-                         .append('<i>' + formatFileSize(data.files[0].size) + '</i>');
-
-            data.context = tpl.appendTo(ul);
-
-//            tpl.find('input').knob();
-
-            tpl.find('span').click(function(){
-
-                if(tpl.hasClass('working')){
-                    jqXHR.abort();
-                }
-
-                tpl.fadeOut(function(){
-                    tpl.remove();
-                });
-
-            });
-
-            var jqXHR = data.submit();
-        },
-        progress: function(e, data){
-
-            var progress = parseInt(data.loaded / data.total * 100, 10);
-            data.context.find('input').val(progress).change();
-
-            if(progress == 100){
-                data.context.removeClass('working');
-            }
-        },
-        fail:function(e, data){
-            data.context.addClass('error');
-        }
-
-    });
-
-    function formatFileSize(bytes) {
-        if (typeof bytes !== 'number') {
-            return '';
-        }
-
-        if (bytes >= 1000000000) {
-            return (bytes / 1000000000).toFixed(2) + ' GB';
-        }
-
-        if (bytes >= 1000000) {
-            return (bytes / 1000000).toFixed(2) + ' MB';
-        }
-
-        return (bytes / 1000).toFixed(2) + ' KB';
+        // let percent = Math.round(e.loaded / e.total * 100);
+        // progressBar.width(percent + '%').html(percent + '%');
+        // progressBar[0].style.width = percent + "%";
     }
+    request.upload.onload = function (e) {
+        console.log("progress completed");
 
-});
+        // progressBar.width(100 + '%').html(100 + '%');
+    }
+    request.open('post', uploadForm.action, true);
+    request.setRequestHeader("Content-Type", "multipart/form-data");
+    request.send(new FormData(uploadForm));
+}
 
 $(document).ready(renderVideosTable);
