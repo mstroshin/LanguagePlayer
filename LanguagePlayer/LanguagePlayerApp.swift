0bb9fd640bcf4e39654a4f512ee18e19dcc90ff9
Maxim Troshin <mstroshin@gmail.com>
2021-07-04 19:15:02 +0300

    Created new project with SwiftUI

---
+++ LanguagePlayer/LanguagePlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  LanguagePlayerApp.swift
+//  LanguagePlayer
+//
+//  Created by Maxim Troshin on 04.07.2021.
+//
+
+import SwiftUI
+
+@main
+struct LanguagePlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
--- LanguagePlayer/Misc/Protocols.swift
+++
@@ -1,9 +0,0 @@
-import Foundation
-
-protocol ViewModel {
-    associatedtype Input
-    associatedtype Output
-    
-    var input: Input { get }
-    var output: Output { get }
-}
